1ef8a3aa716485ad81d051580cea238db3a2c301
SVVHasini <svvhasini@gmail.com>
2024-10-30 19:16:27 +0530

    Fixed my game.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
     flippingCards()
 })
 
-let isFlipping = false;
 let fruits = ["Grapes", "Orange", "Grapes", "Mango"];
 let fruitsJSON = fruits.map((fruit, index) => {
     return { name: fruit, index: index + 1, id: `Card_${index + 1}`, isMatched: false, isFlipped: false }
@@ -11,7 +10,7 @@ let fruitsJSON = fruits.map((fruit, index) => {
 console.log(fruitsJSON)
 let previousItem = ""
 let cardTemplate = (frontSide, backSide, id) => {
-    return `<div class="flip-card" id="${id}">
+    return `<div class="flip-card" id="${id}" onclick="onClickCard(this)">
             <div class="flip-card-inner">
                 <div class="flip-card-front">
                     <span>${frontSide}</span>
@@ -40,18 +39,6 @@ function namingFlipCard(flipCard, index) {
 function flippingCards() {
     let cards = document.querySelectorAll(".flip-card")
     cards.forEach((card, index) => {
-        card.children[0].children[0].addEventListener("click", () => {
-            if (!isFlipping) {
-                isFlipping = true;
-                card.classList.add("clicked");
-                fruitsJSON[index].isFlipped = true;
-                console.log(fruitsJSON)
-                checkingWinCondition()
-                setTimeout(()=>{
-                    isFlipping=false
-                },600)
-            }
-        })
     })
 }
 
@@ -60,22 +47,51 @@ function checkingWinCondition() {
         return fruit.isFlipped;
     })
     if (flippedCards.length == 2) {
+        console.log(flippedCards.length,flippedCards)
         if (flippedCards[0].name == flippedCards[1].name && flippedCards[0].id != flippedCards[1].id) {
-            alert("Won!!");
+            waiting(()=>{alert("Won!!!")})
         }
         else {
-            setTimeout(() => {
+            waiting(()=>{
                 closingCards(flippedCards);
-                isFlipping=false;
-            }, 600);
+            })
         }
     }
+    if(flippedCards.length>2)
+    {
+        waiting(()=>{
+            closingCards(flippedCards);
+        })
+    }
+}
+
+function waiting(slothFN)
+{
+    setTimeout(()=>{
+        slothFN();
+    },600);
 }
 
 function closingCards(flippedCards) {
     flippedCards.forEach((card, index) => {
         let cardElement = document.querySelector(`#${card.id}`)
         cardElement.classList.remove("clicked");
-        fruitsJSON[index].isFlipped = false;
+        let cardIndex=fruitsJSON.findIndex((fruit,index)=>{
+            return card.id==fruit.id;
+        })
+        fruitsJSON[cardIndex].isFlipped = false;
     })
+}
+
+function onClickCard(card){
+        // isFlipping = true;
+        card.classList.add("clicked");
+        let index=(card.getAttribute("id").split("_")[1])-1;
+        fruitsJSON[index].isFlipped = true;
+        // console.log(fruitsJSON)
+        checkingWinCondition()
+        // setTimeout(()=>{
+        //     isFlipping=false
+        // },600)
+        console.log("Clicked")
 }
