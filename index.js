8eb5dc4ed385a58ec778c9cbcc55f071a77ae79d
SVVHasini <svvhasini@gmail.com>
2024-11-08 19:44:57 +0530

    Creating data

--- index.js
+++ index.js
@@ -1,50 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     drawBoard();
 })
-let fruitData = [
-    [
-        { name: 'apple', isflipped: false },
-        { name: 'orange', isflipped: false },
-        { name: 'mango', isflipped: false },
-        { name: 'pear', isflipped: false },
-        { name: 'grapes', isflipped: false },
-    ],
-    [
-        { name: 'apple', isflipped: false },
-        { name: 'orange', isflipped: false },
-        { name: 'mango', isflipped: false },
-        { name: 'pear', isflipped: false },
-        { name: 'grapes', isflipped: false },
-    ],
-    [
-        { name: 'apple', isflipped: false },
-        { name: 'orange', isflipped: false },
-        { name: 'mango', isflipped: false },
-        { name: 'pear', isflipped: false },
-        { name: 'grapes', isflipped: false },
-    ],
-    [
-        { name: 'apple', isflipped: false },
-        { name: 'orange', isflipped: false },
-        { name: 'mango', isflipped: false },
-        { name: 'pear', isflipped: false },
-        { name: 'grapes', isflipped: false },
-    ],
-    [
-        { name: 'apple', isflipped: false },
-        { name: 'orange', isflipped: false },
-        { name: 'mango', isflipped: false },
-        { name: 'pear', isflipped: false },
-        { name: 'grapes', isflipped: false },
-    ]
-]
-let fruits = ["Grapes", "Orange", "Grapes", "Mango"];
-let fruitsJSON = fruits.map((fruit, index) => {
-    return { name: fruit, index: index + 1, id: `Card_${index + 1}`, isMatched: false, isFlipped: false }
-})
-console.log(fruitsJSON)
-let previousItem = ""
-let cardTemplate = (frontSide, backSide, id, size) => {
+var size=2
+var fruitData = []
+var previousItem = ""
+var cardTemplate = (frontSide, backSide, id, size) => {
     return `<div class="flip-card" style="width:${size}px;height:${size}px" id="${id}" onclick="onClickCard(this)">
             <div class="flip-card-inner">
                 <div class="flip-card-front">
@@ -62,8 +22,8 @@ function drawBoard() {
 
 function renderingCards() {
     let container = document.querySelector(".container");
-    let size = fruitData.length;
     let boxsize = "100";
+    creatingData();
     for (let i = 0; i < size; i++) {
         let rowE = document.createElement("div");
         rowE.classList.add("row");
@@ -75,26 +35,35 @@ function renderingCards() {
     }
 }
 
+function creatingData(){
+    let data=[]
+    for(let i=0;i<size;i++)
+    {
+        data.push(allFruits.slice(0,size))
+    }
+    fruitData=data
+}
+
 function checkingWinCondition() {
-    // let flippedCards = fruitsJSON.filter((fruit, index) => {
-    //     return fruit.isFlipped;
-    // })
-    // if (flippedCards.length == 2) {
-    //     console.log(flippedCards.length, flippedCards)
-    //     if (flippedCards[0].name == flippedCards[1].name && flippedCards[0].id != flippedCards[1].id) {
-    //         waiting(() => { alert("Won!!!") })
-    //     }
-    //     else {
-    //         waiting(() => {
-    //             closingCards(flippedCards);
-    //         })
-    //     }
-    // }
-    // if (flippedCards.length > 2) {
-    //     waiting(() => {
-    //         closingCards(flippedCards);
-    //     })
-    // }
+    let flippedCards = fruitsJSON.filter((fruit, index) => {
+        return fruit.isFlipped;
+    })
+    if (flippedCards.length == size) {
+        console.log(flippedCards.length, flippedCards)
+        if (flippedCards[0].name == flippedCards[1].name && flippedCards[0].id != flippedCards[1].id) {
+            waiting(() => { alert("Won!!!") })
+        }
+        else {
+            waiting(() => {
+                closingCards(flippedCards);
+            })
+        }
+    }
+    if (flippedCards.length > size) {
+        waiting(() => {
+            closingCards(flippedCards);
+        })
+    }
     
 }
 
@@ -120,7 +89,310 @@ function onClickCard(card) {
     let indexes=card.getAttribute("id").split(",")
     let indexX = indexes[0];
     let indexY =indexes[1];
-    fruitData[indexX,indexY].isflipped=true;
+    fruitData[indexX][indexY].isflipped=true;
     checkingWinCondition()
     console.log("Clicked")
 }
+
+var allFruits=[
+    {
+      name: 'Apple',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Banana',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Cherry',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Durian',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Elderberry',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Fig',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Grape',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Honeydew Melon',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Indian Gooseberry',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Jackfruit',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Kiwi',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Lemon',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Lime',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Mango',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Nectarine',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Olive',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Papaya',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Quince',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Raspberry',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Strawberry',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Tangerine',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Ugli Fruit',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Vanilla Fruit',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Watermelon',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Xigua Melon',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Yam',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Zucchini',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Blueberry',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Cranberry',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Dragon Fruit',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Guava',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Hazelnut',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Pear',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Plum',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Pineapple',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Persimmon',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Soursop',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Tamarind',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Yuzu',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Acai Berry',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Blackcurrant',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Cloudberry',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Goji Berry',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Juniper Berry',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Apple',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Banana',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Cherry',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Durian',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Elderberry',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    },
+    {
+      name: 'Fig',
+      isMatched: false,
+      isFlipped: false,
+      url: 'https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit-600x600.jpg'
+    }
+  ]
